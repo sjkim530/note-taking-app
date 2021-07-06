c6d71eaf0965aa1f2ffd5f80aa8473ad9070cd2a
Seong Jin Kim <seongjinkimsjk@gmail.com>
2021-07-06 08:15:45 -0400

    editNote function added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const myNotes = document.getElementById("my-notes");
 
 form.addEventListener("submit", addNote);
 
+// Creates a new note
+
 function addNote(e) {
   e.preventDefault();
   let newNote = document.createElement("div");
@@ -11,12 +13,26 @@ function addNote(e) {
   newNote.innerHTML = `
   <button type="button" onclick=deleteNote(this.parentNode)>X</button>
   <p> ${input.value} </p>
-  <button type="button">Edit</button>
+  <button type="button" onclick=editNote(this.parentNode)>Edit</button>
   `;
   myNotes.appendChild(newNote);
   input.value = "";
 }
 
+// Deletes the note when you click on the "X" button
+
 function deleteNote(note) {
   note.remove();
+}
+
+// Edit the note when you click on the "Edit" button
+
+function editNote(note) {
+  const edit = note.querySelector("p");
+  edit.contentEditable = true;
+  edit.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      edit.contentEditable = false;
+    }
+  });
 }
